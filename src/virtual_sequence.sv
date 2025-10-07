3cc3a4a2e67d562d149ecfb91618de67d4caead5
Shashankgg11 <119851778+Shashankgg11@users.noreply.github.com>
2025-10-07 20:07:15 +0530

    Update virtual_sequence.sv

--- src/virtual_sequence.sv
+++ src/virtual_sequence.sv
@@ -4,6 +4,8 @@ class afifo_virtual_seq extends uvm_sequence;
   afifo_virtual_sequencer vseqr;
   afifo_write_seq w_seq;
   afifo_read_seq  r_seq;
+  afifo_w_seq1 w_seq1;
+  afifo_r_seq1 r_seq1;
 
   function new(string name="afifo_virtual_seq");
     super.new(name);
@@ -14,10 +16,18 @@ class afifo_virtual_seq extends uvm_sequence;
 
     w_seq = afifo_write_seq::type_id::create("w_seq");
     r_seq = afifo_read_seq::type_id::create("r_seq");
+    w_seq1 = afifo_w_seq1::type_id::create("w_seq1");
+    r_seq1 = afifo_r_seq1::type_id::create("r_seq1");
 
     fork
-      w_seq.start(vseqr.w_seqr);      
-      r_seq.start(vseqr.r_seqr);
+      begin
+        w_seq.start(vseqr.w_seqr);
+        w_seq1.start(vseqr.w_seqr); 
+      end
+     begin
+       r_seq.start(vseqr.r_seqr);
+       r_seq1.start(vseqr.r_seqr);
+     end
     join
   endtask
 endclass
