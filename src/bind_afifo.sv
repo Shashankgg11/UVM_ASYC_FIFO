f09d3d331b0d2f3cdf47688b57e8a38d1a89609a
Shashankgg11 <119851778+Shashankgg11@users.noreply.github.com>
2025-10-09 22:08:53 +0530

    Create bind_afifo.sv

---
+++ src/bind_afifo.sv
@@ -0,0 +1,12 @@
+bind FIFO afifo_assertions afifo_assert_inst (
+  .wclk(wclk),
+  .rclk(rclk),
+  .wrst_n(wrst_n),
+  .rrst_n(rrst_n),
+  .wdata(wdata),
+  .rdata(rdata),
+  .winc(winc),
+  .rinc(rinc),
+  .wfull(wfull),
+  .rempty(rempty)
+);
