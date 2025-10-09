5631f6247a93481d4277c8ee0c8e9162f55ad1f1
Shashankgg11 <119851778+Shashankgg11@users.noreply.github.com>
2025-10-09 21:56:05 +0530

    Update virtual_sequence.sv

--- src/virtual_sequence.sv
+++ src/virtual_sequence.sv
@@ -7,6 +7,10 @@ class afifo_virtual_seq extends uvm_sequence;
   afifo_w_seq1 w_seq1;
   afifo_r_seq1 r_seq1;
 
+  // control flags
+  bit run_write = 1;
+  bit run_read  = 1;
+
   function new(string name="afifo_virtual_seq");
     super.new(name);
   endfunction
@@ -14,20 +18,24 @@ class afifo_virtual_seq extends uvm_sequence;
   task body();
     $cast(vseqr, m_sequencer);
 
-    w_seq = afifo_write_seq::type_id::create("w_seq");
-    r_seq = afifo_read_seq::type_id::create("r_seq");
-    w_seq1 = afifo_w_seq1::type_id::create("w_seq1");
-    r_seq1 = afifo_r_seq1::type_id::create("r_seq1");
+    if (run_write) begin
+      w_seq  = afifo_write_seq::type_id::create("w_seq");
+      w_seq1 = afifo_w_seq1::type_id::create("w_seq1");
+    end
+    if (run_read) begin
+      r_seq  = afifo_read_seq::type_id::create("r_seq");
+      r_seq1 = afifo_r_seq1::type_id::create("r_seq1");
+    end
 
     fork
-      begin
+      if (run_write) begin
         w_seq.start(vseqr.w_seqr);
-        w_seq1.start(vseqr.w_seqr); 
+        w_seq1.start(vseqr.w_seqr);
       end
-     begin
-       r_seq.start(vseqr.r_seqr);
-       r_seq1.start(vseqr.r_seqr);
-     end
+      if (run_read) begin
+        r_seq.start(vseqr.r_seqr);
+        r_seq1.start(vseqr.r_seqr);
+      end
     join
   endtask
 endclass
