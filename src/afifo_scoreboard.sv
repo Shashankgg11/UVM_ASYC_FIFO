1863499dfb13ae66ab7660019e3fd72226d81b3a
Shashankgg11 <119851778+Shashankgg11@users.noreply.github.com>
2025-10-10 10:28:54 +0530

    Update afifo_scoreboard.sv

--- src/afifo_scoreboard.sv
+++ src/afifo_scoreboard.sv
@@ -1,3 +1,4 @@
+`include "defines.sv"
 `uvm_analysis_imp_decl(_w_in_sb)
 `uvm_analysis_imp_decl(_r_out_sb)
 
@@ -24,39 +25,43 @@ class afifo_scoreboard extends uvm_component;
   	afifo_seq_item#(.DSIZE(8)) item_copy;
   	item_copy = afifo_seq_item#(.DSIZE(8))::type_id::create("item_copy");
   	item_copy.copy(t);
-    if(t.wfull == 0)begin
-  		ref_q.push_back(item_copy);
-        `uvm_info(get_type_name(),$sformatf("WRITE_IN: wdata=%0h (queue size=%0d)",item_copy.wdata, ref_q.size()),UVM_LOW)
-    end
     
-    else `uvm_info(get_type_name(),$sformatf("wfull is high while writing"),UVM_LOW)
-      
+    if(t.winc)begin
+      if(ref_q.size() < `DEPTH)begin
+        ref_q.push_back(item_copy);
+        `uvm_info(get_type_name(),$sformatf("WRITE_IN: wdata=%0h (queue size=%0d)",item_copy.wdata, ref_q.size()),UVM_LOW)
+      end
+      else begin
+        if (t.wfull) `uvm_info(get_full_name(),"FIFO FULL signal is correct",UVM_MEDIUM)
+        else `uvm_info(get_full_name(),"FIFO FULL signal is not correct",UVM_MEDIUM)  
+      end
+     end
+          
     `uvm_info(get_type_name(), $sformatf("\n***********************************************************\n\n"), UVM_LOW)   
   endfunction
 
   function void write_r_out_sb(afifo_seq_item t);
-    if (ref_q.size() == 0) begin
-      `uvm_error(get_type_name(), "READ_OUT when queue empty")
-    end
-    
-    else if(t.rinc ==1)begin
-      afifo_seq_item exp = ref_q.pop_front();
-      `uvm_info(get_type_name(),$sformatf("Expected wdata =%0d, Got rdata =%0d rempty ",exp.wdata, t.rdata, t.rempty),UVM_LOW)
-      
-      if (exp.rempty == 1)`uvm_info(get_type_name(), "rempty is high", UVM_LOW)
         
-        else if (exp.wdata !== t.rdata)begin
-          mismatch++;
-          `uvm_error(get_type_name(), $sformatf("Mismatch! Expected=%0d Got=%0d", exp.wdata, t.rdata))
-        end
+      if(t.rinc)begin
+        if(ref_q.size() > 0)begin
+          afifo_seq_item exp = ref_q.pop_front();
+          `uvm_info(get_type_name(),$sformatf("READ (queue size=%0d)",ref_q.size()),UVM_LOW)
+          if (exp.wdata === t.rdata)begin
+            match++;
+            `uvm_info(get_type_name(), $sformatf("match! Expected=%0d Got=%0d", exp.wdata, t.rdata), UVM_LOW)
+          end
       
+          else begin
+            mismatch++;
+            `uvm_error(get_type_name(), $sformatf("Mismatch! Expected=%0d Got=%0d", exp.wdata, t.rdata))
+          end
+        end
         else begin
-          match++;
-          `uvm_info(get_type_name(), $sformatf("match! Expected=%0d Got=%0d", exp.wdata, t.rdata), UVM_LOW)
+          if(t.rempty) `uvm_info(get_full_name(),"FIFO EMPTY signal is correct",UVM_MEDIUM)
+          else `uvm_info(get_full_name(),"FIFO EMPTY signal is not correct",UVM_MEDIUM)  
         end
-    end
-      
-    else `uvm_info(get_type_name(), $sformatf("rinc is zero while reading"), UVM_LOW)
+     end
+          
     
     `uvm_info(get_type_name(), $sformatf("\n MATCH = %0d   MISMATCH = %0d", match, mismatch), UVM_LOW)   
     `uvm_info(get_type_name(), $sformatf("\n***********************************************************\n \n***********************************************************\n \n"), UVM_LOW)   
